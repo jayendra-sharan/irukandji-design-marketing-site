31464045d94158666633215454c8f28268bdd45d
Jayendra Sharan <jayendra.sharan@gmail.com>
2025-04-09 14:50:10 +0000

    Update basepath

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,14 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  base: "irukandji-design-marketing-site",
+  build: {
+    outDir: "./docs",
+    emptyOutDir: true, // also necessary
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
